6cee37678f3da578db95162edee95a9abfa8b979
Atharv724 <98732673+Atharv724@users.noreply.github.com>
2022-10-06 19:53:46 +1300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preLoad(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    webcam = createCapture(VIDEO);
+    webcam.size(300, 300);
+    webcam.hide();
+}
+
+function draw(){
+image(webcam, 0, 0, 300, 300);
+}
